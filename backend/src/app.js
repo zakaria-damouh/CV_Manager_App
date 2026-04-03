50d11be54a16a053aba4aadfc4f272307485d02b
zakaria-damouh <damouh.zakaria28@gmail.com>
2026-04-03 16:14:21 +0200

    feat: set up express middleware with helmet and cors for security

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
+
+app.use(helmet());
+app.use(cors({origin: process.env.CLIENT_ID}));
+app.use(express.json());
 
 const PORT = process.env.PROT || 5000;
 
