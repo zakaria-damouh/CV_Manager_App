864e54435a8901248a55c61c744488bcdb34b869
fechtalimarouane <marwanefechtali@gmail.com>
2026-04-08 22:03:39 +0100

    conecte backend avec frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(helmet());
-app.use(cors({origin: process.env.CLIENT_ID}));
+app.use(cors({origin: process.env.CLIENT_URL}));
 app.use(express.json());
 
 app.use('/api/auth/' , authRoutes);
